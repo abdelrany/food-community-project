513b196d82e34f52f14c583890858eb35612370a
abdelghani salama <abdelrany10@gmail.com>
2023-01-26 21:57:06 +0100

    feat(food-community-project): deploy in firebase

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="footer-container">
+
+
+            <p className="footer-title"> abdelrany's Foodary © 2023</p>
+
+        </div>
+    );
+}
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,39 @@
+export const my_recipe = {
+    recipe_1: {
+        name: 'Chirashi Don',
+        preparation_time: '30 minutes',
+        servings: '2',
+        image: "/img/recipe1.jpg",
+        ingredients: [
+            '1/2 cup rice',
+            '1/2 cup water',
+            '2 eggs',
+            '300g of salmon, tuna and salmon roe',
+        ],
+        instructions: [
+            'Cook rice and water in a rice cooker',
+            'Fry eggs in a pan',
+            'Serve rice, eggs and fish on a plate',
+        ],
+
+    }, recipe_2: {
+        name: 'Mexi-Chicken Avocado Cups',
+        preparation_time: '10 minutes',
+        servings: '3',
+        image: "/img/recipe2.jpg",
+        ingredients: [
+
+            '3 (6 ounce) cans canned chicken, drained',
+            '1 tablespoon cilantro, finely chopped',
+            '½ teaspoon chili powder, or more to tastes',
+            '3 avocados, halved lengthwise and pitted',
+            '1 teaspoon lime juice, or to taste',
+        ],
+        instructions: [
+            'Stir chicken, cilantro, and chili powder together in a bowl.',
+            'Arrange avocado halves on serving dish and brush with lime juice. Spoon chicken mixture into avocado halves.',
+
+        ],
+
+    },
+}
